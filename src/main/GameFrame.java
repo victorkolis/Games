f9a327c668bc923cee6f0b1571bb68868f465494
Victor Kolis <>
2020-11-03 21:49:34 -0300

    20201103

---
+++ src/main/GameFrame.java
@@ -0,0 +1,11 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+	
+	public GameFrame() {
+		
+	}
+
+}
---
+++ src/main/GamePanel.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener {
+	
+	public GamePanel() {
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
