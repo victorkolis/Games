9b442a6a4adfe5a6bc5d1197209c03181a712552
Victor Kolis <>
2020-11-03 22:24:26 -0300

    20201103

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,20 +1,81 @@
 package main;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
+
+	static final int SCREEN_WIDTH = 600;
+	static final int SCREEN_HEIGHT = 600;
+	static final int UNIT_SIZE = 25;
+	static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT)/UNIT_SIZE;
+	static final int DELAY = 75;
+	// Holds the coordinates for the body parts of the snake including the head
+	final int x[] = new int[GAME_UNITS];
+	final int y[] = new int[GAME_UNITS];
+	int bodyParts = 6;
+	int applesEaten;
+	int appleX; 
+	int appleY;
+	char direction = 'R'; // The letters stand for the position the snake is going, R(right); L(left); U(up); D(down)
+	boolean running = false;
+	Timer timer;
+	Random random;
 	
-	public GamePanel() {
+	GamePanel() {
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
+
+	}
+	public void startGame() {
+		newApple();
+		running = true;
+		timer = new Timer(DELAY, this);
+		timer.start();
+
+	}
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+	}
+	public void draw() {
+
+	}
+	public void newApple() {
 		
 	}
+	public void move() {
+
+	}
+	public void checkApple() {
+
+	}
+	public void checkCollisions() {
+
+	}
+	public void gameOver(Graphics g) {
+
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
+	public class MyKeyAdapter extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent e) {
+
+
+		}
+	}
+
 }
