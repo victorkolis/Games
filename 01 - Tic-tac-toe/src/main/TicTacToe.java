50ce8173f38d2f832755c00262765441b2231e0d
Victor Kolis <>
2020-11-07 11:02:42 -0300

    20201107

--- 01 - Tic-tac-toe/src/main/TicTacToe.java
+++ 01 - Tic-tac-toe/src/main/TicTacToe.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 
 public class TicTacToe implements ActionListener{
 
-	
+
 	Random random = new Random();
 	JFrame frame = new JFrame();
 	JPanel title_panel = new JPanel();
@@ -15,7 +15,9 @@ public class TicTacToe implements ActionListener{
 	JLabel textfield = new JLabel();
 	JButton[] buttons = new JButton[9];
 	boolean player1_turn;
-	
+	String xSignature = "X";
+	String oSignature = "O";
+
 	public TicTacToe() {
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,32 +25,32 @@ public class TicTacToe implements ActionListener{
 		frame.getContentPane().setBackground(new Color(100,100,100));
 		frame.setLayout(new BorderLayout());
 		frame.setVisible(true);
-		
+
 		textfield.setBackground(new Color(23,23,23));
-		textfield.setForeground(new Color(25, 255, 0));
-		textfield.setFont(new Font("Georgia", Font.BOLD, 75));
+		textfield.setForeground(new Color(255, 255, 0));
+		textfield.setFont(new Font("Campus Personal Use", Font.BOLD, 75));
 		textfield.setHorizontalAlignment(JLabel.CENTER);
 		textfield.setText("Tic-tac-toe");
 		textfield.setOpaque(true);
-		
+
 		title_panel.setLayout(new BorderLayout());
-		title_panel.setBounds(0, 0, 700, 50);
-		
+		title_panel.setBounds(0, 0, 700, 30);
+
 		for(int i = 0; i < 9; i++) {
 			buttons[i] = new JButton();
 			button_panel.add(buttons[i]);
-			buttons[i].setFont(new Font("Georgia", Font.BOLD, 110));
+			buttons[i].setFont(new Font("Universidad", Font.BOLD, 110));
 			buttons[i].setFocusable(false);
 			buttons[i].addActionListener(this);
 		}
-		
+
 		button_panel.setLayout(new GridLayout(3,3));
-		button_panel.setBackground(new Color(250,100,00));
-		
+		button_panel.setBackground(new Color(10,10,250));
+
 		title_panel.add(textfield);
 		frame.add(title_panel, BorderLayout.NORTH);
 		frame.add(button_panel);
-		
+
 		firstTurn();
 	}
 
@@ -60,7 +62,16 @@ public class TicTacToe implements ActionListener{
 				if(player1_turn) {
 					if(buttons[i].getText() == "") {
 						buttons[i].setForeground(Color.BLACK);
-						buttons[i].setText("X");
+						buttons[i].setText(this.xSignature);
+						player1_turn = false;
+						textfield.setText(this.oSignature);
+					}
+				} else {
+					if(buttons[i].getText() == "") {
+						buttons[i].setForeground(Color.RED);
+						buttons[i].setText(this.oSignature);
+						player1_turn = true;
+						textfield.setText(this.xSignature);
 					}
 				}
 			}
@@ -69,20 +80,20 @@ public class TicTacToe implements ActionListener{
 	}
 
 	public void firstTurn() {
-		
+
 		try {
 			Thread.sleep(4000);
 		} catch (InterruptedException e) {
-			
+
 			e.printStackTrace();
 		}
-		
+
 		if(random.nextInt(2) == 0) {
 			player1_turn = true;
-			textfield.setText("X turn");
+			textfield.setText(this.xSignature);
 		} else {
 			player1_turn = false;
-			textfield.setText("O turn");
+			textfield.setText(this.oSignature);
 		}
 
 	}
